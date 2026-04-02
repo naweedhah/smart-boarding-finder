bcb5752a6426e78076f2da196836e4668fd51371
SakithNizar <sakithmohamed54@gmail.com>
2026-04-02 23:51:33 +0530

    feat: link inquiry approval to chat creation

--- api/src/features/inquiry/inquiry.controller.js
+++ api/src/features/inquiry/inquiry.controller.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 let inquiries = [];
 
 exports.createInquiry = (req, res) => {
@@ -35,4 +37,26 @@ exports.createInquiry = (req, res) => {
   inquiries.push(inquiry);
 
   res.json(inquiry);
+};
+
+
+exports.acceptInquiry = async (req, res) => {
+  const { id } = req.params;
+
+  const inquiry = {
+    id,
+    studentId: "student1",
+    ownerId: "owner1"
+  };
+
+  const chatRes = await axios.post("http://localhost:5000/api/sakith-chat/create", {
+    inquiryId: id,
+    studentId: inquiry.studentId,
+    ownerId: inquiry.ownerId
+  });
+
+  res.json({
+    message: "Inquiry approved and chat created",
+    chat: chatRes.data
+  });
 };
